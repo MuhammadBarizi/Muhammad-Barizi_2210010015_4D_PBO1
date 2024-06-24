3f0590edb5144ad67ffc7be23872a07679564cae
MuhammadBarizi <141615878+MuhammadBarizi@users.noreply.github.com>
2024-06-24 17:54:58 +0800

    Update Orang.java

--- Orang.java
+++ Orang.java
@@ -1,1 +1,14 @@
+public class Orang {
+    String nama;
+    int umur;
 
+    public Orang(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+        System.out.println("Nama orang ini " + this.nama);
+        System.out.println("Umur orang ini " + this.umur);
+    }
+    
+    
+    
+}
